907fc6831f7d9d486a5e76a8dc93612401430960
Domitrius Clark <domitriusaclark@gmail.com>
2022-08-10 04:12:36 -0400

    Next commits

---
+++ _eleventy/config.js
@@ -0,0 +1,23 @@
+
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+
+
+module.exports = function (eleventyConfig) {
+  // Serverless
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "events",
+    functionsDir: './netlify/functions',
+    redirects: "netlify-toml-builders"
+  });
+
+  return {
+    templateFormats: ["njk",],
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_includes"
+    },
+    htmlTemplateEngine: `njk`,
+    dataTemplateEngine: `njk`,
+  }
+}
